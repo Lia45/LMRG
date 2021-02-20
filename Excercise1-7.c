aab62ad178ea1379ab81ac064602c4ae3382a0f1
Lia45 <78566540+Lia45@users.noreply.github.com>
2021-02-20 02:45:41 -0600

    Excercise 1-7

---
+++ Excercise1-7.c
@@ -0,0 +1,10 @@
+/*Excercise 1-7, section 1.5.1, page 17*/
+/*Write a program to print the value of EOF*/
+
+#include <stdio.h>
+
+int main(){
+    
+    printf("Value of EOF: %d\n", EOF);
+    return 0;
+}
