eb044aae49490021e6dcd6627486bc85c4802abd
Lia45 <78566540+Lia45@users.noreply.github.com>
2021-02-22 03:26:33 -0600

    Add files via upload

---
+++ Excercise1-11.c
@@ -0,0 +1,7 @@
+/*Excercise 1-11, Section 1.5.4, page 21*/
+
+/*How would you test the word count program?*/
+/*Using numbers or signs instead of words*/
+
+/* What kinds of input are most likely to uncover bugs if there are any?*
+/*Really huge word, white spaces, no input*/
